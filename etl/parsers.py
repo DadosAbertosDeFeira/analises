77cbd95c6cf320742024cce13e36999835cbab4b
Ana Paula Gomes <apgomes88@gmail.com>
2020-06-13 10:46:06 +0200

    Adiciona método para converter de valor em string para float

---
+++ etl/parsers.py
@@ -0,0 +1,7 @@
+def currency_to_float(value):
+    """Converte de R$ 69.848,70 (str) para 69848.70 (float)."""
+    try:
+        cleaned_value = value.replace("R$", "").replace(".", "").replace(",", ".")
+        return float(cleaned_value)
+    except ValueError:
+        return
---
+++ etl/tests/test_parsers.py
@@ -0,0 +1,17 @@
+from etl.parsers import currency_to_float
+import pytest
+
+
+@pytest.mark.parametrize(
+    "original_value,expected_value",
+    [
+        ("R$ 69.848,70", 69848.70),
+        ("69.848,70", 69848.70),
+        ("R$ -69.848,70", -69848.70),
+        ("1,70", 1.70),
+        ("00,00", 0),
+        ("Random", None),
+    ],
+)
+def test_currency_to_float(original_value, expected_value):
+    assert currency_to_float(original_value) == expected_value
