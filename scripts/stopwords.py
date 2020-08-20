51e68e79a823a6afe71be095daa81e100bbed30c
Jkasnese <guilhermelw.uefs@gmail.com>
2020-08-20 12:28:20 -0300

    Resolve import das stopwords customizadas

---
+++ scripts/stopwords.py
@@ -0,0 +1,58 @@
+PRONOUNS = [
+    "desta",
+    "destas",
+    "deste",
+    "destes",
+    "qualquer",
+    "das",
+    "seguintes"
+]
+
+ROMAN_NUMERALS = [
+    "i",
+    "ii",
+    "iii",
+    "iv",
+    "v",
+    "vi",
+    "vii",
+    "viii",
+    "ix",
+    "xx",
+    "xxi"
+    "xxii",
+    "xxiii",
+    "xiv",
+    "xv",
+    "xvi",    
+    "xvii",
+    "xviii",
+    "xix",
+    "xx"
+]
+
+LETTERS = [
+    "b",
+    "c",
+    "d",
+    "f",
+    "g",
+    "h",
+    "j",
+    "k",
+    "l",
+    "m",
+    "n",
+    "p",
+    "q",
+    "r",
+    "s",
+    "t",
+    "u",
+    "w",
+    "x",
+    "y",
+    "z",
+]
+
+CUSTOM_STOPWORDS = PRONOUNS + ROMAN_NUMERALS + LETTERS
