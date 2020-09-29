6cdbfe4488a8b5056489d13299ff8aad95e73fab
Jkasnese <guilhermelw.uefs@gmail.com>
2020-09-29 17:04:58 -0300

    Adiciona parser de processo_licitatorio

--- scripts/parsers.py
+++ scripts/parsers.py
@@ -5,3 +5,35 @@ def currency_to_float(value):
         return float(cleaned_value)
     except ValueError:
         return
+
+def limpa_proc_licitatorio(proc: str) -> str:
+    """Corrige typos no campo PROCESSO LICITATORIO no dataset de pagamentos
+    Retorna o tipo do processo licitatorio em minúsculo"""
+    
+    isento = ['ISENTO', 'ENTO', 'Isento', 'SENTO', 'sento']
+    pregao = ['PREGAO', 'REGAO', 'Pregao', 'EGAO', 'egao']
+    dispensa = ['DISPENSA', 'SPENSA', 'ISPENSA', 'Dispensa', 'PENSA', 'pensa']
+    concorrencia = ['CONCORENCIA', 'ONCORRENCIA', 'NCORRENCIA']
+    inexigibilidade = ['INEXIGIBILIDADE', 'NEXIGIBILIDADE', 'Inexibilidade', 'XIGIBILIDADE',
+     'xigibilidade', 'EXIGIBILIDADE', 'exigibilidade']
+    tomada_de_preco = ['TOMADA DE PRECO', 'OMADA DE PRECO', 'omada de preco']
+
+    if (type(proc) != str):
+        return None
+    try:
+        if (proc in isento):
+            return 'isento'
+        elif (proc in pregao):
+            return 'pregao'
+        elif (proc in dispensa):
+            return 'dispensa'
+        elif (proc in concorrencia):
+            return 'concorrencia'
+        elif (proc in inexigibilidade):
+            return 'inexigibilidade'
+        elif (proc in tomada_de_preco):
+            return 'tomada de preco'
+        else:
+            return proc.lower()
+    except TypeError:
+        pass
--- scripts/tests/test_parsers.py
+++ scripts/tests/test_parsers.py
@@ -1,4 +1,4 @@
-from scripts.parsers import currency_to_float
+from scripts.parsers import currency_to_float, limpa_proc_licitatorio
 import pytest
 
 
@@ -15,3 +15,40 @@ import pytest
 )
 def test_currency_to_float(original_value, expected_value):
     assert currency_to_float(original_value) == expected_value
+
+@pytest.mark.parametrize(
+    "proc_licitatorio_sujo,proc_licitatorio_limpo",
+    [
+        ("ISENTO", "isento"),
+        ("ENTO", "isento"),
+        ("Isento","isento"),
+        ("SENTO","isento"),
+        ("sento","isento"),
+        ("PREGAO","pregao"),
+        ("REGAO","pregao"),
+        ("Pregao","pregao"),
+        ("EGAO","pregao"),
+        ("egao","pregao"),
+        ("DISPENSA","dispensa"),
+        ("SPENSA","dispensa"),
+        ("ISPENSA","dispensa"),
+        ("Dispensa","dispensa"),
+        ("PENSA","dispensa"),
+        ("pensa","dispensa"),
+        ("CONCORENCIA","concorrencia"),
+        ("ONCORRENCIA","concorrencia"),
+        ("NCORRENCIA","concorrencia"),
+        ("INEXIGIBILIDADE","inexigibilidade"),
+        ("NEXIGIBILIDADE","inexigibilidade"),
+        ("Inexibilidade","inexigibilidade"),
+        ("XIGIBILIDADE","inexigibilidade"),
+        ("xigibilidade","inexigibilidade"),
+        ("EXIGIBILIDADE","inexigibilidade"),
+        ("exigibilidade","inexigibilidade"),
+        ("TOMADA DE PRECO","tomada de preco"),
+        ("OMADA DE PRECO","tomada de preco"),
+        ("omada de preco","tomada de preco"),
+    ],
+)
+def test_limpa_proc_licitatorio(proc_licitatorio_sujo, proc_licitatorio_limpo):
+    assert limpa_proc_licitatorio(proc_licitatorio_sujo) == proc_licitatorio_limpo
