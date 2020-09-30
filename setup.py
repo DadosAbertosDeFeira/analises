6f7d164a63dc13c36db53661d441ff5af1ae8a30
Jkasnese <guilhermelw.uefs@gmail.com>
2020-09-30 17:24:53 -0300

    Reformata setup.py

--- setup.py
+++ setup.py
@@ -3,7 +3,5 @@ from distutils.core import setup
 from setuptools import find_packages
 
 setup(
-    name="analises",
-    version="0.1",
-    packages=find_packages(),
+    name="analises", version="0.1", packages=find_packages(),
 )
