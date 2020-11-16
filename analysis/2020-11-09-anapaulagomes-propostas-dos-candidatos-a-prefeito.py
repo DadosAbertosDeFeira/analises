74b104ddf21a1aeff8362e64a42a32cd46569b32
Ana Paula Gomes <apgomes88@gmail.com>
2020-11-16 15:02:57 +0100

    Melhora cores, adiciona stopwords e possibilita reprodutibilidade

--- analysis/2020-11-09-anapaulagomes-propostas-dos-candidatos-a-prefeito.py
+++ analysis/2020-11-09-anapaulagomes-propostas-dos-candidatos-a-prefeito.py
@@ -10,6 +10,14 @@
 # Através de uma iniciativa colaborativa, coletamos os links e extraímos
 # o texto das propostas. Veja mais sobre isso no [Dados de planos de governo de candidatos às prefeituras dos municípios em 2020](https://dadosabertos.social/t/dados-de-planos-de-governo-de-candidatos-as-prefeituras-dos-municipios-em-2020/645/1).
 #
+# Para rodar essa análise você deve:
+# * Baixar o arquivo [propostas.7z](https://github.com/augusto-herrmann/eleicoes-2020-planos-de-governo/blob/main/dados/propostas.7z)
+# * Extraí-lo na pasta `analysis` ou modificar a variável `proposals_directory` com o caminho completo para a pasta
+#
+# Padrão de pastas esperado: `propostas > sigla do estado > cidades`
+#
+# ---
+#
 # Nessa análise vamos visualizar uma nuvem de palavras das propostas a
 # candidatos de toda Bahia e de Feira de Santana.
 #
@@ -19,7 +27,7 @@
 # 2. Quais são as palavras de destaque entre todos os candidatos a prefeito de Feira juntos?
 # 3. Quais são as palavras de destaque entre todos os candidatos a prefeito da Bahia?
 
-# In[19]:
+# In[3]:
 
 
 import os
@@ -33,18 +41,25 @@ from scripts.nlp import remove_portuguese_stopwords
 from texthero import preprocessing
 from wordcloud import WordCloud
 
+target_state = "ba"  # sigla do estado em minúsculo
+target_city = "feira-de-santana"  # nome da cidade separado por hífens
+proposals_directory = "propostas/"
 proposals_and_cities = {}
 
-# estrutura: cidade > proposta-<candidato>.txt
-for folder, _, files in os.walk("BA"):
+# estrutura: estado > cidade > proposta-<candidato>.txt
+for folder, _, files in os.walk(proposals_directory):
     if files:
-        proposals_and_cities[folder] = files
-proposals_and_cities["BA/feira-de-santana"]
+        index_city = folder.rfind("/")
+        city = folder[index_city + 1 :]
+        index_state = folder[:index_city].rfind("/")
+        state = folder[index_state + 1 : index_city]
+        proposals_and_cities[f"{state}/{city}"] = files
+proposals_and_cities[f"{target_state}/{target_city}"]
 
 
 # # Propostas por candidato
 
-# In[20]:
+# In[6]:
 
 
 custom_pipeline = [
@@ -54,29 +69,43 @@ custom_pipeline = [
 ]
 
 candidates_and_proposals = {}
-directory = "BA/feira-de-santana"
-for file_name in proposals_and_cities[directory]:
+for file_name in proposals_and_cities[f"{target_state}/{target_city}"]:
     candidate_name = file_name[12:]
     candidate_name = candidate_name.replace(".txt", "")
     candidate_name = candidate_name.replace("-", " ")
     candidate_name = candidate_name.title()
     print(candidate_name)
     content = open(f"{directory}/{file_name}").read()
-    custom_stop_words = ["feira", "santana", "municipio", "municipal", "município"]
+    custom_stop_words = [
+        "feira",
+        "santana",
+        "municipio",
+        "municipal",
+        "município",
+        "municipais",
+        "cidade",
+    ]
     cleaned_text = remove_portuguese_stopwords(content, custom_stop_words)
     cleaned_text = pd.Series(cleaned_text)
     cleaned_text = hero.clean(cleaned_text, custom_pipeline)
     candidates_and_proposals[candidate_name] = cleaned_text
 
 
-# In[21]:
+# In[7]:
+
+
+# In[8]:
 
 
 for candidate, proposal in candidates_and_proposals.items():
     print(f"------------------------ {candidate}")
     text = " ".join(proposal)
     wordcloud = WordCloud(
-        background_color="white", width=2000, height=800, collocations=False
+        background_color="white",
+        width=2000,
+        height=800,
+        colormap="PuOr",
+        collocations=False,
     ).generate(text)
     fig = plt.figure(figsize=(20, 10))
     plt.imshow(wordcloud, interpolation="bilinear")
@@ -96,7 +125,7 @@ for candidate, proposal in candidates_and_proposals.items():
 #
 # ## O que acontece se juntarmos todos?
 
-# In[22]:
+# In[9]:
 
 
 all_proposals = []
@@ -122,7 +151,7 @@ plt.show()
 #
 # O que será que as propostas de todos os candidatos a prefeito dizem?
 
-# In[23]:
+# In[10]:
 
 
 custom_pipeline = [
@@ -142,13 +171,13 @@ for city, proposals in proposals_and_cities.items():
         all_proposals.extend(cleaned_text)
 
 
-# In[24]:
+# In[11]:
 
 
 text = " ".join(all_proposals)
 
 
-# In[25]:
+# In[12]:
 
 
 wordcloud = WordCloud(
